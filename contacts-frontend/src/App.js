b8119ec366a19e3c18a6de033a63ffd75fdcc243
Enrique Guerrero <enriqueguefer@gmail.com>
2019-12-29 02:21:48 +0100

    Componentes de Alertas, Contactos y Contacto

--- contacts-frontend/src/App.js
+++ contacts-frontend/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Contacts from './Contacts.js'
 
 function App() {
+
+  const contacts = [
+    {
+      name: "Robert",
+      phone: "66500614"
+    },
+    {
+      name: "Marc",
+      phone: "673386565"
+    },
+    {
+      name: "Elissa",
+      phone: "661456789"
+    },
+    {
+      name: "Anthony",
+      phone: "668123789"
+    },
+    {
+      name: "John",
+      phone: "665123462"
+    }
+  ];
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Contacts contacts =  {contacts}/>
+    </div>
   );
 }
 
